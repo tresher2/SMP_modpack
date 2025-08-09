53a71ccb9b45f27c00edc608d938e826862f281a
tresher <tema149.tema149@gmail.com>
2025-08-09 23:28:03 +0700

    hb

--- minecraft/kubejs/server_scripts/example.js
+++ minecraft/kubejs/server_scripts/example.js
@@ -1,4 +1,14 @@
 ServerEvents.recipes(event => {
+    event.shaped('tacz:target', [
+        ' A ',
+        ' B ',
+        ' C '
+    ], {
+        A: 'create:iron_sheet',
+        B: 'create:rose_quartz_lamp',
+        C: 'quark:stripped_oak_post'
+    })
+
     event.recipes.createMechanicalCrafting(Item.of('tacz:modern_kinetic_gun', '{GunId:"create_armorer:pistol_revolver_torque"}'), [
         'ABC',
         ' DE'
@@ -6,7 +16,38 @@ ServerEvents.recipes(event => {
         A: 'createimmersivetacz:gunbarrel',
         B: 'create:precision_mechanism',
         C: 'createimmersivetacz:firing_mechanism',
-        D: 'createimmersivetacz:firing_mechanism',
+        D: 'createimmersivetacz:gun_trigger',
+        E: 'create:andesite_casing'
+    })
+
+    event.recipes.createMechanicalCrafting(Item.of('tacz:modern_kinetic_gun', '{GunId:"create_armorer:pistol_auto_stress"}'), [
+        '  C ',
+        'ABCD',
+        '  EF'
+    ], {
+        A: 'createimmersivetacz:gunbarrel',
+        B: 'create:brass_block',
+        C: 'createimmersivetacz:firing_mechanism',
+        H: 'create:precision_mechanism',
+        D: 'create:sequenced_gearshift',
+        E: 'createimmersivetacz:gun_trigger',
+        À: 'create:brass_casing'
+    })
+
+    event.recipes.createMechanicalCrafting(Item.of('tacz:modern_kinetic_gun', '{GunId:"create_armorer:rifle_assult_crane"}'), [
+        'BBCD ',
+        'GGAFS',
+        '  TE '
+    ], {
+        G: 'createimmersivetacz:gunbarrel',
+        F: 'createimmersivetacz:firing_mechanism',
+        T: 'createimmersivetacz:gun_trigger',
+        S: 'create:shaft',
+        A: 'tfmg:steel_mechanism',
+        L: 'minecraft:leather',
+        B: 'create:brass_sheet',
+        C: 'create:sturdy_sheet',
+        D: 'create:electron_tube',
         E: 'create:andesite_casing'
     })
 })
