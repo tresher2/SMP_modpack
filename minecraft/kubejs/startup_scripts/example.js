8eee8182eaf2a79694787b23a482f0b08155e053
tresher <tema149.tema149@gmail.com>
2025-08-12 17:49:08 +0700

    тест + текстуры

--- minecraft/kubejs/startup_scripts/example.js
+++ minecraft/kubejs/startup_scripts/example.js
@@ -3,4 +3,10 @@
 // Visit the wiki for more info - https://kubejs.com/
 
 console.info('Hello, World! (Loaded startup scripts)')
-
+StartupEvents.registry('item', event => {
+    event.create('incomplete_throwable_hole_filler').displayName('Incomplete Hole filler').maxStackSize(32)
+    event.create('incomplete_throwable_hole_filler_balanced').displayName('Incomplete filler - Choise').maxStackSize(32)
+    event.create('incomplete_throwable_hole_filler_balanced_slice').displayName('Incomplete filler - Slice').maxStackSize(32)
+    event.create('incomplete_throwable_hole_filler_smart').displayName('Incomplete filler - Smart').maxStackSize(32)
+    event.create('incomplete_throwable_hole_filler_water').displayName('Incomplete filler - Water').maxStackSize(32)
+})
